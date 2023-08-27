fb54d9da861b33a8d5b3013a03c8d7b0eb22adef
Ashraf Hassan <112810015+AshrafHassan95@users.noreply.github.com>
2023-08-27 23:38:35 +0800

    Update openweather_data_pipeline.py

--- Automated-Data-Pipeline-Airflow/openweather_data_pipeline.py
+++ Automated-Data-Pipeline-Airflow/openweather_data_pipeline.py
@@ -1,1 +1,97 @@
+from airflow import DAG
+from datetime import timedelta, datetime
+from airflow.providers.http.sensors.http import HttpSensor
+import json
+from airflow.providers.http.operators.http import SimpleHttpOperator
+from airflow.operators.python import PythonOperator
+import pandas as pd
 
+
+def kelvin_to_celsius(temp_in_kelvin):
+    temp_in_celsius = (temp_in_kelvin - 273.15)
+    return temp_in_celsius
+
+
+def transform_load_data(task_instance):
+    data = task_instance.xcom_pull(task_ids="extract_weather_data")
+    city = data["name"]
+    weather_description = data["weather"][0]['description']
+    temp_celsius = kelvin_to_celsius(data["main"]["temp"])
+    feels_like_celsius= kelvin_to_celsius(data["main"]["feels_like"])
+    min_temp_celsius = kelvin_to_celsius(data["main"]["temp_min"])
+    max_temp_celsius = kelvin_to_celsius(data["main"]["temp_max"])
+    pressure = data["main"]["pressure"]
+    humidity = data["main"]["humidity"]
+    wind_speed = data["wind"]["speed"]
+    time_of_record = datetime.utcfromtimestamp(data['dt'] + data['timezone'])
+    sunrise_time = datetime.utcfromtimestamp(data['sys']['sunrise'] + data['timezone'])
+    sunset_time = datetime.utcfromtimestamp(data['sys']['sunset'] + data['timezone'])
+
+    transformed_data = {"City": city,
+                        "Description": weather_description,
+                        "Temperature (C)": temp_celsius,
+                        "Feels Like (C)": feels_like_celsius,
+                        "Minimun Temp (C)":min_temp_celsius,
+                        "Maximum Temp (C)": max_temp_celsius,
+                        "Pressure": pressure,
+                        "Humidty": humidity,
+                        "Wind Speed": wind_speed,
+                        "Time of Record": time_of_record,
+                        "Sunrise (Local Time)":sunrise_time,
+                        "Sunset (Local Time)": sunset_time                        
+                        }
+    transformed_data_list = [transformed_data]
+    df_data = pd.DataFrame(transformed_data_list)
+    aws_credentials = {"key": "xxxxxxxxx", "secret": "xxxxxxxxxx", "token": "xxxxxxxxxxxxxx"}
+
+    now = datetime.now()
+    dt_string = now.strftime("%d%m%Y%H%M%S")
+    dt_string = 'current_weather_data_portland_' + dt_string
+    df_data.to_csv(f"s3://weatherapiairflowbucket-ash/{dt_string}.csv", index=False, storage_options=aws_credentials)
+
+
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2023, 1, 8),
+    'email': ['ashraf950901@gmail.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 2,
+    'retry_delay': timedelta(minutes=2),
+}
+
+
+
+with DAG('weather_dag',
+        default_args=default_args,
+        schedule_interval = '@daily',
+        catchup=False) as dag:
+
+
+        is_weather_api_ready = HttpSensor(
+        task_id ='is_weather_api_ready',
+        http_conn_id='weathermap_api',
+        endpoint='/data/2.5/weather?q={city name}&appid={API key}'
+        )
+
+
+        extract_weather_data = SimpleHttpOperator(
+        task_id = 'extract_weather_data',
+        http_conn_id = 'weathermap_api',
+        endpoint='/data/2.5/weather?q={city name}&appid={API key}',
+        method = 'GET',
+        response_filter= lambda r: json.loads(r.text),
+        log_response=True
+        )
+
+        transform_load_weather_data = PythonOperator(
+        task_id= 'transform_load_weather_data',
+        python_callable=transform_load_data
+        )
+
+
+
+
+        is_weather_api_ready >> extract_weather_data >> transform_load_weather_data
